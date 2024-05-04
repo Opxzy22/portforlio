2fc95ccc7b687a206aefd0003a65f02b3f0c6a30
opxzy22 <aopeyem84@gmail.com>
2024-05-04 23:01:21 +0100

    showcase my skills

--- my-app/src/Components/about.js
+++ my-app/src/Components/about.js
@@ -21,6 +21,25 @@ function About() {
             learning and a collaborative mindset, I thrive in dynamic environments where 
             innovation and creativity are valued.
             </p>
+            <p>
+              During my one-year software engineering program at Alx, I underwent intensive 
+              training aimed at tackling challenging tasks. This experience has ingrained 
+              in me the belief that overcoming difficulties is an integral part of everyday 
+              life. Consequently, I now approach challenges with a sense of normalcy, always 
+              striving for success. This mindset fuels my continuous hunger for personal and 
+              professional growth. By embracing challenges head-on and refusing to surrender 
+              in the face of adversity, I have consistently improved in my endeavors. When 
+              confronted with a particularly tough task, I opt for brief pauses to recharge 
+              before returning with renewed determination. This approach has proven instrumental 
+              in my journey of skill development and achievement.
+              In addition, my time at Alx instilled in me not only technical expertise but also a 
+              collaborative spirit. Working closely with peers and mentors, I learned the value of 
+              teamwork and effective communication. These soft skills have complemented my technical 
+              abilities, enabling me to navigate complex projects with ease. Furthermore, the program 
+              fostered a culture of innovation and creativity, encouraging me to think outside the box and 
+              explore unconventional solutions. This approach to software engineering has equipped me with a 
+              versatile skill set, ready to tackle any challenge that comes my way.
+            </p>
 
           </Fade>
           </div>
