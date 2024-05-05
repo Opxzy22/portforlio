5207cb22b47dbf1af010be1ffbf9e273c3229a0d
opxzy22 <aopeyem84@gmail.com>
2024-05-05 16:38:54 +0100

    showcase my skills

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,17 @@
-import ReactDOM from 'react-dom/client';
-import App from './App.js';
+import React from 'react';
+import ReactDOM from 'react-dom'; // Import ReactDOM from 'react-dom' directly
 import './index.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const queryClient = new QueryClient();
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <QueryClientProvider client={queryClient}>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-    <ReactQueryDevtools />
-  </QueryClientProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
